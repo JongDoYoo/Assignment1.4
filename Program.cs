4513ac9699b981df71e9e74eb029acde44f8a194
JongD <JongD@JONGDO>
2024-05-09 08:38:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point();
+            Point point2 = new Point();
+
+            if (point1.x == point2.x)
+            {
+                Console.WriteLine("Points are ib same axis");
+            }
+            else
+            {
+                Console.WriteLine("Points not on same axis");
+            }
+        }
+        
+        
+
+    }
+    public struct Point
+    {
+        double x;
+        double y;
+
+    }
+}
+    
+
